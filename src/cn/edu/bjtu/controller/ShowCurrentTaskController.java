5986bf7d05d70b6982a1b452c2643be1d02da031
solitudeycq <solitudeycq@163.com>
2015-12-22 00:54:08 +0800

    【bug修复】将showdrivertask分解为showcurrenttask、shownewtask、showfinishedtask三个接口，方便安卓端调用

---
+++ src/cn/edu/bjtu/controller/ShowCurrentTaskController.java
@@ -0,0 +1,60 @@
+package cn.edu.bjtu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+import cn.edu.bjtu.service.DriverService;
+import cn.edu.bjtu.service.OrderService;
+import cn.edu.bjtu.vo.Driverinfo;
+import cn.edu.bjtu.vo.Orderform;
+import net.sf.json.JSONArray;
+
+@Controller
+public class ShowCurrentTaskController {
+	
+	@Autowired
+	DriverService driverService;
+	@Autowired
+	Driverinfo driverinfo;
+	@Autowired
+	OrderService orderService;
+	
+	@RequestMapping(value="/showcurrenttask",produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String showCurrentTask(@RequestParam(value="phone",required=false) String phone){
+		
+		String f = "false";
+		String s = "待收货";
+		//获取司机名字，以便搜索订单表
+		//System.out.println(phone);
+		driverinfo = driverService.getDriverByPhone(phone);
+		String name = driverinfo.getDriverName();
+		//System.out.println(name);
+		
+		//将List集合转化为JSONArray.
+		List<Orderform> l = orderService.getOrderByDriverName(name);
+		//筛选符合要求的任务
+		for(int i=0;i < l.size(); i++){
+			Orderform o = (Orderform)l.get(i);
+			if((f.equals(o.getConfirm()))||(!(s.equals(o.getState())))){
+				l.remove(i);
+				i=i-1;
+			}
+		}
+		JSONArray orderResult = JSONArray.fromObject(l);
+		//System.out.println(orderResult);
+		//Orderform o = (Orderform)l.get(0);
+		//String test = o.toString();
+		//System.out.println(test);
+		
+		return orderResult.toString();
+	}
+
+}
---
+++ src/cn/edu/bjtu/controller/ShowFinishedTaskController.java
@@ -0,0 +1,53 @@
+package cn.edu.bjtu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+import cn.edu.bjtu.service.DriverService;
+import cn.edu.bjtu.service.OrderService;
+import cn.edu.bjtu.vo.Driverinfo;
+import cn.edu.bjtu.vo.Orderform;
+import net.sf.json.JSONArray;
+
+@Controller
+public class ShowFinishedTaskController {
+	
+	@Autowired
+	DriverService driverService;
+	@Autowired
+	Driverinfo driverinfo;
+	@Autowired
+	OrderService orderService;
+	
+	@RequestMapping(value="/showfinishedtask",produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String showFinishedTask(@RequestParam(value="phone",required=false) String phone){
+		
+		String f = "已完成";
+		//获取司机名字，以便搜索订单表
+		driverinfo = driverService.getDriverByPhone(phone);
+		String name = driverinfo.getDriverName();
+		
+		//将List集合转化为JSONArray.
+		List<Orderform> l = orderService.getOrderByDriverName(name);
+		
+		//筛选符合要求的任务
+		for(int i=0;i < l.size(); i++){
+			Orderform o = (Orderform)l.get(i);
+			if(!(f.equals(o.getState()))){
+				l.remove(i);
+				i=i-1;
+			}
+		}
+		JSONArray orderResult = JSONArray.fromObject(l);
+		return orderResult.toString();
+	}
+
+}
