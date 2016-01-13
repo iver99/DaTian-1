0cfa76cd292bab8a641d295bee8924f621c7ead3
iver99 <chendonghao_cn@163.com>
2016-01-13 11:18:01 +0800

    重写clientDao和clientSecurityDao

--- src/cn/edu/bjtu/dao/ClientSecurityDao.java
+++ src/cn/edu/bjtu/dao/ClientSecurityDao.java
@@ -3,15 +3,6 @@ package cn.edu.bjtu.dao;
 import cn.edu.bjtu.vo.Userinfo;
 
 public interface ClientSecurityDao {
-	public boolean checkOldPassword(String oldPassword,String userId);
-	
-	public boolean changePassword(String newPassword,String userId);
-	
-	public Userinfo getUserById(String userId);
-	
-	
-	public boolean setSecurityQuestion(String q1,String q2,String q3,String a1,String a2,String a3,String uId);
-	public boolean checkAnswer(String a1,String a2,String a3,String userId);
 
 
 }
--- src/cn/edu/bjtu/dao/impl/ClientDaoImpl.java
+++ src/cn/edu/bjtu/dao/impl/ClientDaoImpl.java
@@ -21,35 +21,5 @@ import cn.edu.bjtu.vo.Userinfo;
  */
 public class ClientDaoImpl extends BaseDaoImpl<Clientinfo> implements ClientDao {
 
-	@Autowired
-	UserinfoDao userinfoDao;
-
-
-	@Override
-	public boolean validateUser(String userId, String realName, String phone,
-			String IDCard, String sex, String path, String fileName) {
-		
-		Clientinfo clientInfo = this.get(Clientinfo.class, userId);
-		if (clientInfo == null) {// clientinfo找不到记录
-			return false;
-		}
-
-		clientInfo.setRealName(realName);
-		clientInfo.setPhone(phone);
-		clientInfo.setIdcard(IDCard);
-		clientInfo.setSex(sex);
-		this.update(clientInfo);
-		Userinfo userInfo = userinfoDao.get(Userinfo.class, userId);
-		userInfo.setStatus("审核中");
-		if (path != null && fileName != null) {
-			String fileLocation = path + "//" + fileName;
-			clientInfo.setIDPicture(fileLocation);//设置文件上传路径
-		}
-		userinfoDao.update(userInfo);
-
-		// }
-
-		return true;
-	}
 
 }
