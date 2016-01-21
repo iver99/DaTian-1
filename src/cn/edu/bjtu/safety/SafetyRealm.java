186f6c08cc7e5908fb2f08b3e71e1d65531e1f3f
iver99 <chendonghao_cn@163.com>
2016-01-21 17:26:46 +0800

    【config】**引入shiro安全框架，用于权限控制等一系列功能**

---
+++ src/cn/edu/bjtu/safety/SafetyRealm.java
@@ -0,0 +1,33 @@
+package cn.edu.bjtu.safety;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+@Component
+public class SafetyRealm  extends AuthorizingRealm{
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.AuthorizingRealm#doGetAuthorizationInfo(org.apache.shiro.subject.PrincipalCollection)
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(
+			AuthenticationToken arg0) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
+	
