bf0b328b44cd6f84734b201f96e5cc7d3ac123f3
solitudeycq <solitudeycq@163.com>
2016-03-27 16:55:45 +0800

    【bug修复】删除导出对账单内无用的信息

--- src/cn/edu/bjtu/util/ExcelCreator.java
+++ src/cn/edu/bjtu/util/ExcelCreator.java
@@ -20,32 +20,24 @@ public class ExcelCreator {
         //创建要显示的内容,创建一个单元格，第一个参数为列坐标，第二个参数为行坐标，第三个参数为内容
         Label orderNum = new Label(0,0,"订单编号");
         sheet.addCell(orderNum);
-        Label clientName = new Label(1,0,"客户名称");
-        sheet.addCell(clientName);
-        Label companyName = new Label(2,0,"承运方");
+        Label companyName = new Label(1,0,"承运方");
         sheet.addCell(companyName);
-        Label contractId = new Label(3,0,"承运方合同编号");
-        sheet.addCell(contractId);
-        Label submitTime = new Label(4,0,"订单完成时间");
+        Label submitTime = new Label(2,0,"订单完成时间");
         sheet.addCell(submitTime);
-        Label expectedPrice = new Label(5,0,"订单运费(元)");
+        Label expectedPrice = new Label(3,0,"订单运费(元)");
         sheet.addCell(expectedPrice);
-        Label actualPrice = new Label(6,0,"最终运费(元)");
+        Label actualPrice = new Label(4,0,"最终运费(元)");
         sheet.addCell(actualPrice);
         
         Label orderNum_data = new Label(0,1,settlement.getOrderNum());
         sheet.addCell(orderNum_data);
-        Label clientName_data = new Label(1,1,settlement.getClientName());
-        sheet.addCell(clientName_data);
-        Label companyName_data = new Label(2,1,settlement.getCompanyName());
+        Label companyName_data = new Label(1,1,settlement.getCompanyName());
         sheet.addCell(companyName_data);
-        Label contractId_data = new Label(3,1,settlement.getContractId());
-        sheet.addCell(contractId_data);
-        Label submitTime_data = new Label(4,1,settlement.getSubmitTime().toString());
+        Label submitTime_data = new Label(2,1,settlement.getSubmitTime().toString());
         sheet.addCell(submitTime_data);
-        Label expectedPrice_data = new Label(5,1,settlement.getExpectedPrice().toString());
+        Label expectedPrice_data = new Label(3,1,settlement.getExpectedPrice().toString());
         sheet.addCell(expectedPrice_data);
-        Label actualPrice_data = new Label(6,1,settlement.getActualPrice().toString());
+        Label actualPrice_data = new Label(4,1,settlement.getActualPrice().toString());
         sheet.addCell(actualPrice_data);
         
         //把创建的内容写入到输出流中，并关闭输出流
@@ -62,34 +54,26 @@ public class ExcelCreator {
         //创建要显示的内容,创建一个单元格，第一个参数为列坐标，第二个参数为行坐标，第三个参数为内容
         Label orderNum = new Label(0,0,"订单编号");
         sheet.addCell(orderNum);
-        Label clientName = new Label(1,0,"客户名称");
-        sheet.addCell(clientName);
-        Label companyName = new Label(2,0,"承运方");
+        Label companyName = new Label(1,0,"承运方");
         sheet.addCell(companyName);
-        Label contractId = new Label(3,0,"承运方合同编号");
-        sheet.addCell(contractId);
-        Label submitTime = new Label(4,0,"订单完成时间");
+        Label submitTime = new Label(2,0,"订单完成时间");
         sheet.addCell(submitTime);
-        Label expectedPrice = new Label(5,0,"订单运费(元)");
+        Label expectedPrice = new Label(3,0,"订单运费(元)");
         sheet.addCell(expectedPrice);
-        Label actualPrice = new Label(6,0,"最终运费(元)");
+        Label actualPrice = new Label(4,0,"最终运费(元)");
         sheet.addCell(actualPrice);
         
         for(int i=0;i<multipleStatement.size();i++)
         {
         	Label orderNum_data = new Label(0,i+1,multipleStatement.get(i).getOrderNum());
             sheet.addCell(orderNum_data);
-            Label clientName_data = new Label(1,i+1,multipleStatement.get(i).getClientName());
-            sheet.addCell(clientName_data);
-            Label companyName_data = new Label(2,i+1,multipleStatement.get(i).getCompanyName());
+            Label companyName_data = new Label(1,i+1,multipleStatement.get(i).getCompanyName());
             sheet.addCell(companyName_data);
-            Label contractId_data = new Label(3,i+1,multipleStatement.get(i).getContractId());
-            sheet.addCell(contractId_data);
-            Label submitTime_data = new Label(4,i+1,multipleStatement.get(i).getSubmitTime().toString());
+            Label submitTime_data = new Label(2,i+1,multipleStatement.get(i).getSubmitTime().toString());
             sheet.addCell(submitTime_data);
-            Label expectedPrice_data = new Label(5,i+1,multipleStatement.get(i).getExpectedPrice().toString());
+            Label expectedPrice_data = new Label(3,i+1,multipleStatement.get(i).getExpectedPrice().toString());
             sheet.addCell(expectedPrice_data);
-            Label actualPrice_data = new Label(6,i+1,multipleStatement.get(i).getActualPrice().toString());
+            Label actualPrice_data = new Label(4,i+1,multipleStatement.get(i).getActualPrice().toString());
             sheet.addCell(actualPrice_data);
         }
         //把创建的内容写入到输出流中，并关闭输出流
