f7a3c9e801928971796489759930cd505f96ef7d
admin <admin@admin-PC>
2016-05-22 11:20:18 +0800

    【功能完善】：配置不同的文件格式检查

--- WebContent/views/js/uploadFileCheck.js
+++ WebContent/views/js/uploadFileCheck.js
@@ -1,7 +1,7 @@
 /**
  * check the extention of uploaded file
  */
-
+//文档和图片都可以上传
 function fileChange(value){
 		var file_ext=new Array();
 		file_ext[0]="zip";
@@ -14,8 +14,6 @@ function fileChange(value){
 		file_ext[8]="gif";
 		file_ext[9]="bmp";
 		file_ext[10]="jpeg";
-		
-		
 		ext=value.split(".")[1];
 		for(x in file_ext){
 			if( ext== file_ext[x]){
@@ -28,3 +26,43 @@ function fileChange(value){
 		alert("文件格式不正确，只能上传zip,doc,docx,txt,xls,jpg,png,jpeg,gif,bmp格式！");
         return false;
 	}
+
+//只能上传图片 
+function fileChangePicOnly(value){
+	var file_ext=new Array();
+	file_ext[0]="jpg";
+	file_ext[1]="png";
+	file_ext[2]="gif";
+	file_ext[3]="bmp";
+	file_ext[4]="jpeg";
+	ext=value.split(".")[1];
+	for(x in file_ext){
+		if( ext== file_ext[x]){
+			return true;
+		}
+	}
+	//格式不符合，清空文件信息并提示用户
+	$("#upload_btn4").val(undefined);
+	$("#apply_attachment1").val("");
+	alert("文件格式不正确，只能上传jpg,png,jpeg,gif,bmp格式！");
+    return false;
+}
+function fileChangeDocOnly(value){
+	var file_ext=new Array();
+	file_ext[0]="zip";
+	file_ext[1]="doc";
+	file_ext[3]="docx";
+	file_ext[4]="xls";
+	file_ext[5]="txt";
+	ext=value.split(".")[1];
+	for(x in file_ext){
+		if( ext== file_ext[x]){
+			return true;
+		}
+	}
+	//格式不符合，清空文件信息并提示用户
+	$("#upload_btn4").val(undefined);
+	$("#apply_attachment1").val("");
+	alert("文件格式不正确，只能上传zip,doc,docx,txt,xls格式！");
+    return false;
+}
